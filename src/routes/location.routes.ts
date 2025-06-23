9ecd3c16cd325c63acb6c792736ddf773dbb5d14
izza-sirkast <dawlaizza405@gmail.com>
2025-06-23 20:37:38 +0700

    feat: find locations

--- src/routes/location.routes.ts
+++ src/routes/location.routes.ts
@@ -1,40 +1,30 @@
-import express from 'express';
+import { Router } from 'express';
 import { Request, Response } from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-// Routes
-import locationsRoutes from './routes/location.routes';
-
 dotenv.config();
 
-const app = express();
-app.use(express.json());
+const router = Router();
 
-app.use('/locations', locationsRoutes);
+router.get('/find', async (req: Request, res: Response) => {
+    const { q } = req.query;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
-});
-
-app.get('/country', async (req: Request, res: Response) => {
     try {
         const response = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete`,
             {
                 params: {
-                    q: 'ban',
+                    q,
                     apikey: process.env.ACCUWEATHER_API_KEY,
                 }
             }
         );
         console.log('Data fetched successfully:', response.data);
-        res.json(response.data);
+        res.status(200).json(response.data);
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Failed to fetch data' });
     }
-});
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
 })
+
+export default router;
